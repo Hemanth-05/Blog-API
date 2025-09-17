f630f9606e0d4c85691c1ffad3042cb62f924c00
Hemanth-05 <hemanthnath123@gmail.com>
2025-09-16 23:39:30 -0400

    Added custom error handeller middlewear

--- src/server.js
+++ src/server.js
@@ -9,7 +9,14 @@ app.use(morgan('tiny'));
 app.use(express.json());
 app.use("/api/blogs", blogRoutes);
 
-
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    if(!err.status){
+        err.status = 500;
+    }
+    err.message = `Internal Server Error`;
+    res.status(err.status).json({message: err.message});
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server is running in port ${PORT}`);
